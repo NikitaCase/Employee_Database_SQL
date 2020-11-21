b72ec760c3cccbc4c4245480ed37e6cb6d8d1d35
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2020-11-21 15:33:28 -0500

    queries
    Queries.sql added
    ERD txt
    ERD image
    schema pretified
    pics for readme

--- schema.sql
+++ schema.sql
@@ -1,44 +1,46 @@
-CREATE TABLE departments(
+CREATE TABLE Departments(
 	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
 	dept_name VARCHAR(40)
 );
 
 
-CREATE TABLE titles(
+CREATE TABLE Titles(
 	title_id VARCHAR (5) PRIMARY KEY, 
 	title VARCHAR(50)
 );
 
 
-CREATE TABLE employees(
+CREATE TABLE Employees(
 	emp_no INT PRIMARY KEY NOT NULL, 
 	emp_title_id VARCHAR(5),
 	birth_date DATE,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
-	sex VARCHAR(1)
+	sex VARCHAR(1),
 	hire_date DATE, 
-	FOREIGN KEY emp_title_id REFERENCES titles(title_id)
+	FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
 );
+ 
 
-
-CREATE TABLE dept_employee(
-	emp_no INT PRIMARY KEY NOT NULL,
+CREATE TABLE Dept_Employees(
+	emp_no INT,
 	dept_no VARCHAR(4),
-	FOREIGN KEY emp_no REFERENCES employees(emp_no),
-	FOREIGN KEY dept_no REFERENCES departments(dept_no)
+	PRIMARY KEY(emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
 
 
-CREATE TABLE dept_manager(
-	dept_no VARCHAR(4)PRIMARY KEY NOT NULL,
-	emp_no INT
-	FOREIGN KEY emp_no REFERENCES employees(emp_no),
-	FOREIGN KEY dept_no REFERENCES departments(dept_no)
+CREATE TABLE Dept_Manager(
+	dept_no VARCHAR(4),
+	emp_no INT PRIMARY KEY NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
 
 
-CREATE TABLE salary(
+CREATE TABLE Salaries(
 	emp_no INT PRIMARY KEY,
-	salary INT 
+	salary INT, 
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
